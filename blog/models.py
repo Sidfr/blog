56fe7b978afb66ea71f5fedee227b21cf62f746b
MACBOOK <said.idbaih@gmail.com>
2020-05-30 17:41:12 +0200

    authent

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
-from django.db import models
+#from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.urls import reverse
+
 
 
 class Post(models.Model):
@@ -14,3 +16,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
